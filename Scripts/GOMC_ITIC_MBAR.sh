77ef01193b15d82f006a335f4d78e75ff7677de4
mostafa-razavi <sr87@zips.uakron.edu>
2019-08-01 08:42:16 -0600

    Implemented the capability to run GOMC_ITIC_MBAR.sh only for certain ITIC points. If "all" then all ITIC points are considered. Needs to be tested in Chem20.

--- Scripts/GOMC_ITIC_MBAR.sh
+++ Scripts/GOMC_ITIC_MBAR.sh
@@ -8,6 +8,8 @@ target_par=$2
 Nsnapshots=$3
 Ncores=$4
 GOMC_exe=$5
+Selected_Ts=$6
+Selected_rhos=$7
 
 # Create an array of reference force fields' names
 IFS=', ' read -r -a ref_ff_array <<< "$ref_ff_string"
@@ -25,6 +27,44 @@ do
 done
 parallel_file_name="${parallel_file_name}${target_ff_name}.target"
 
+# Function that returns 1 if the T_rho pair is selected 
+isFolderSelected () {
+
+	path_string=$1
+	T_array=$2
+	rho_array=$3
+
+	# If temperature or density array is "all" it means that all ITIC points are selected, so we return 1
+	if [ "$T_array" == "all" ] || [ "$rho_array" == "all" ]; then 
+		return 1
+	fi
+
+	T_array=($T_array)
+	rho_array=($rho_array)
+
+	IFS='/' read -ra IX_Y_Z <<< "$path_string"
+	IT_or_IC=${IX_Y_Z[-3]}
+	rho_or_T1=${IX_Y_Z[-2]}
+	rho_or_T2=${IX_Y_Z[-1]}
+
+	if [ "$IT_or_IC" == "IC" ]; then
+		rho=$rho_or_T1
+		T=$rho_or_T2
+	fi
+	if [ "$IT_or_IC" == "IT" ]; then
+		rho=$rho_or_T2
+		T=$rho_or_T1
+	fi
+
+
+	for i in $(seq 0 1 $(echo "${#rho_array[@]}-1" | bc))	# Loop from 0 to len(rho_array)-1
+	do
+		if [ "$rho" == "${rho_array[i]}" ] && [ "$T" == "${T_array[i]}" ]; then
+  			return 1
+		fi
+	done
+}
+
 # Prepare the .rer files in parallel
 rm -rf $parallel_file_name.parallel
 for j in ${all_ff_array[@]}
@@ -32,15 +72,18 @@ do
     for i in ${ref_ff_array[@]}
     do
         for k in $CD/$i/I*/*/*/
-        do
-            if [ "$j" == "$target_ff_name" ]
-            then
-                rerun_par_address="$CD/${target_par}"
-            else
-                rerun_par_address=$(ls $CD/$j/Files/*.par)
+        do 
+            isFolderSelected "$k" "$Selected_Ts" "$Selected_rhos"
+	        if [ "$?" == "1" ]; then
+                if [ "$j" == "$target_ff_name" ]
+                then
+                    rerun_par_address="$CD/${target_par}"
+                else
+                    rerun_par_address=$(ls $CD/$j/Files/*.par)
+                fi
+                # Arguments: Nsnapshots=$1 GOMC_PDB=$2 rerun_par_address=$3 output_name=$4             
+                echo "cd $k; bash /home/mostafa/Git/TranSFF/Scripts/GOMC_Rerun.sh $Nsnapshots nvt_BOX_0.pdb ${rerun_par_address} $i.ref_$j.rer ${GOMC_exe}" >> $parallel_file_name.parallel
             fi
-            # Arguments: Nsnapshots=$1 GOMC_PDB=$2 rerun_par_address=$3 output_name=$4             
-            echo "cd $k; bash /home/mostafa/Git/TranSFF/Scripts/GOMC_Rerun.sh $Nsnapshots nvt_BOX_0.pdb ${rerun_par_address} $i.ref_$j.rer ${GOMC_exe}" >> $parallel_file_name.parallel
         done
     done
 done
@@ -53,20 +96,23 @@ for i in ${ref_ff_array[@]}
 do
     for k in $CD/$i/I*/*/*/
     do     
-        if [ "$i" == "${ref_ff_array[0]}" ]
-        then
-            ref_sim_fol_string=""
-            for l in ${ref_ff_array[@]}
-            do
-                string=$(echo "$k" | sed "s/$i/$l/")
-                ref_sim_fol_string="${ref_sim_fol_string} ${string}"
-            done
+        isFolderSelected "$k" "$Selected_Ts" "$Selected_rhos"
+        if [ "$?" == "1" ]; then    
+            if [ "$i" == "${ref_ff_array[0]}" ]
+            then
+                ref_sim_fol_string=""
+                for l in ${ref_ff_array[@]}
+                do
+                    string=$(echo "$k" | sed "s/$i/$l/")
+                    ref_sim_fol_string="${ref_sim_fol_string} ${string}"
+                done
 
-            Temp=$(grep -R "Temperature" $k/nvt.inp| awk '{print $2}')
-            which_datafile_columns_string="1 2"   #Total_En Press (0 is first column)
-            how_many_datafile_rows_to_skip="0"
-            energy_unit="K"
-            python3.5 /home/mostafa/Git/TranSFF/Scripts/MBAR_predict.py "$Temp" "$Nsnapshots" "$ref_sim_fol_string" "$ref_ff_string" "$target_ff_name" "$which_datafile_columns_string" "$how_many_datafile_rows_to_skip" "$energy_unit" >> "$CD/${parallel_file_name}.res"
+                Temp=$(grep -R "Temperature" $k/nvt.inp| awk '{print $2}')
+                which_datafile_columns_string="1 2"   #Total_En Press (0 is first column)
+                how_many_datafile_rows_to_skip="0"
+                energy_unit="K"
+                python3.5 /home/mostafa/Git/TranSFF/Scripts/MBAR_predict.py "$Temp" "$Nsnapshots" "$ref_sim_fol_string" "$ref_ff_string" "$target_ff_name" "$which_datafile_columns_string" "$how_many_datafile_rows_to_skip" "$energy_unit" >> "$CD/${parallel_file_name}.res"
+            fi
         fi
     done
 done
--- Scripts/RunGrid.sh
+++ Scripts/RunGrid.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # This script runs GOMC_ITIC_MBAR.sh script for specified sigma and epsilons as two arrays by modifying the raw_par file.
 raw_par="/home/mostafa/Git/TranSFF/Forcefields/C2_sSOMEeSOME.par"
+Selected_Ts="274.79 235.56 360.00"
+Selected_rhos="0.0857 0.4714 0.6000"
 sig=(3.730 3.740 3.750 3.760 3.770)
 eps=(109 113 117 121 125)
 for isig in "${sig[@]}"
@@ -11,7 +13,7 @@ do
     cp  $raw_par "s${isig}e${ieps}".par
     sed -i "s/some_sig/$isig/g" "s${isig}e${ieps}".par
     sed -i "s/some_eps/$ieps/g" "s${isig}e${ieps}".par
-    bash ~/Git/TranSFF/Scripts/GOMC_ITIC_MBAR.sh 's3.650e117  s3.675e117  s3.700e117  s3.725e117  s3.750e117  s3.775e117  s3.800e117  s3.825e117  s3.850e117' s${isig}e${ieps}.par 1000 24 ~/Git/GOMC/GOMC-FSHIFT2-HighPrecisionPDB-StartFrame/bin/GOMC_CPU_NVT
+    bash ~/Git/TranSFF/Scripts/GOMC_ITIC_MBAR.sh 's3.650e117  s3.675e117  s3.700e117  s3.725e117  s3.750e117  s3.775e117  s3.800e117  s3.825e117  s3.850e117' s${isig}e${ieps}.par 1000 24 ~/Git/GOMC/GOMC-FSHIFT2-HighPrecisionPDB-StartFrame/bin/GOMC_CPU_NVT $Selected_Ts $Selected_rhos
 
     done
 done

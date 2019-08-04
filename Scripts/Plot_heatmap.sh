989c547e1d814d85eff46bb524d968e3f47771d2
mostafa-razavi <sr87@zips.uakron.edu>
2019-08-04 04:30:03 -0600

    Split the Plot_heatmap_or_zu.sh into Plot_heatmap.sh and Plot_zu.sh

---
+++ Scripts/Plot_heatmap.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+# This script plots a deviation heatmap as a function of sigma and epsilon. Deviation is calculated 
+# by Compare_MBAR_and_direct_sim.py script with respect to true data (either direct simualtion or experimental data).
+# z_wt and u_wt are given as command-line argument (any number between 0 and 1) where the sum should be 1.
+# Last two arguments are sigma and epsilon increments on x and y axis of the heatmap.
+# Examples:
+# bash ~/Git/TranSFF/Scripts/Plot_heatmap.sh 30.07 target.res CassandraRdr.res 0.5Z_0.5U_all.txt 0.5 0.5 0.002 2
+
+MW=$1
+mbar_file_name_tail_keyword=$2
+true_data_file=$3
+outfile=$4
+z_wt=$5
+u_wt=$6	
+sig_increment=$7
+eps_increment=$8
+
+if [ ! -e $outfile ]; then
+	for i in *.$mbar_file_name_tail_keyword
+	do
+		cat $i | awk '{print $2, $3, $4, $5, $6}' > $i.temp
+		plot_or_deviation="deviation"
+		score=$(python3.6 ~/Git/TranSFF/Scripts/Compare_MBAR_and_direct_sim.py $plot_or_deviation $true_data_file $i.temp $i.png $MW $z_wt $u_wt)
+		rm $i.temp
+
+		head=${i::-11}
+		tail=${head:136}
+		sig=${tail::-4} 
+		eps=${tail:6}
+		echo $sig $eps $score >> $outfile
+	done
+else
+	echo "$outfile already exists. The data will be replotted anyway."
+fi
+
+min_sig=$(cut -d " " -f1 $outfile | sort -n | head -n1)
+max_sig=$(cut -d " " -f1 $outfile | sort -n | tail -n1)
+min_eps=$(cut -d " " -f2 $outfile | sort -n | head -n1)
+max_eps=$(cut -d " " -f2 $outfile | sort -n | tail -n1)
+
+if [ "$sig_increment" == "" ]; then
+	sig_increment="0.001"
+fi
+if [ "$eps_increment" == "" ]; then
+	eps_increment="1.0"
+fi
+
+gnuplot -persist << PLOT
+	set terminal pngcairo size 750,750 enhanced font "Times,16"
+	set termoption dashe
+	set output '${outfile}.png'
+	set autoscale x
+	set autoscale y
+
+	set ylabel "{/Symbol s} [A]"
+	set xlabel "{/Symbol e}/k_B [K]"
+
+	#set palette rgbformula -7,2,8
+	#set cbrange [0:100]
+
+	set xtics $eps_increment out
+	set ytics $sig_increment out
+
+	set xrange[$min_eps:$max_eps]
+	set yrange[$min_sig:$max_sig]
+	
+	plot '$outfile' using 2:1:3 with image
+	
+	unset key
+PLOT
---
+++ Scripts/Plot_zu.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# This script plots (Z-1)/rho vs and TUres vs 1000/T plot side by side
+# Example:
+# bash ~/Git/TranSFF/Scripts/Plot_zu.sh 30.07 target.res CassandraRdr.res
+
+MW=$1
+mbar_file_name_tail_keyword=$2
+true_data_file=$3
+
+for i in *.$mbar_file_name_tail_keyword
+do
+	cat $i | awk '{print $2, $3, $4, $5, $6}' > $i.temp
+	plot_or_deviation="plot"
+	score=$(python3.6 ~/Git/TranSFF/Scripts/Compare_MBAR_and_direct_sim.py $plot_or_deviation $true_data_file $i.temp $i.png $MW)
+	rm $i.temp
+done

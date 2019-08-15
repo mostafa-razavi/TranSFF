636649e5f3920c375128cde5cb212099369453c1
mostafa-razavi <sr87@zips.uakron.edu>
2019-08-15 19:28:28 -0400

    RunGrid.sh now support more than one true data

--- Scripts/RunGrid.sh
+++ Scripts/RunGrid.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This script runs GOMC_ITIC_MBAR.sh script for specified sigma and epsilons as two arrays by modifying the raw_par file.
+
 RunGrid_name="RunGrid_sig3.740-3.770_eps116.5-124_lines-1-2-8-10-19-23-26-27_ref-s3.725e117-s3.750e117-s3.775e117"
 reference_foldernames_array="s3.725e117"
 Ncore="24"
@@ -14,7 +15,7 @@ eps=(116.5 117.0)
 
 # Plot_heatmap arguments
 MW="30.06904"
-true_data_file="$HOME/Git/TranSFF/Data/C2/GONvtRdr_select7.res"                                                               
+true_data_file_array="$HOME/Git/TranSFF/Data/C2/GONvtRdr_select7.res $HOME/Git/TranSFF/Data/C2/REFPROP_select7.res"                                                               
 true_data_label="TraPPE-UA"
 mbar_file_name_tail_keyword="target.res"
 z_wt="0.5"
@@ -48,14 +49,20 @@ do
     done
 done
 
-bash $HOME/Git/TranSFF/Scripts/Plot_heatmap.sh $MW $true_data_file $mbar_file_name_tail_keyword $z_wt $u_wt $heatmap_outfile $sig_increment $eps_increment
-bash $HOME/Git/TranSFF/Scripts/Plot_zu.sh $MW $true_data_file $mbar_file_name_tail_keyword $true_data_label
+true_data_file_array=($true_data_file_array)
+for i in $(seq 0 $((${#true_data_file_array[@]}-1)))
+do 
+echo ${true_data_file_array[i]}
+    bash $HOME/Git/TranSFF/Scripts/Plot_heatmap.sh $MW ${true_data_file_array[i]} $mbar_file_name_tail_keyword $z_wt $u_wt $heatmap_outfile $sig_increment $eps_increment
+    bash $HOME/Git/TranSFF/Scripts/Plot_zu.sh $MW ${true_data_file_array[i]} $mbar_file_name_tail_keyword $true_data_label
+done
+
 
 mkdir $RunGrid_name 
 
 cp $0 $RunGrid_name
 mv $heatmap_outfile $RunGrid_name
-cp $true_data_file $RunGrid_name
+for i in $(seq 0 $((${#true_data_file_array[@]}-1))); do cp $true_data_file $RunGrid_name; done
 mv *.parallel $RunGrid_name
 mv *.png $RunGrid_name 
 mv *.res $RunGrid_name

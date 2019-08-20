4e0cf8712f1b42ad04e03c552c2d5078c18b6e55
mostafa-razavi <sr87@zips.uakron.edu>
2019-08-20 01:13:04 -0400

    Organized the par files in Forcefields directry

--- Scripts/RunGrid.sh
+++ Scripts/RunGrid.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 # This script runs GOMC_ITIC_MBAR.sh script for specified sigma and epsilons as two arrays by modifying the raw_par file.
+RunGrid_name="RunGrid_CH3-s3.760e120.5-CH2-s3.980-4.020e53-63_select7_ref-all"
 
-RunGrid_name="RunGrid_sig3.740-3.770_eps116.5-124_lines-1-2-8-10-19-23-26-27_ref-s3.725e117-s3.750e117-s3.775e117"
-reference_foldernames_array="s3.725e117"
-Ncore="24"
+reference_foldernames_array="s3.700e123-s4.100e58 s3.725e123-s4.075e58 s3.750e123-s4.050e58 s3.775e123-s4.025e58 s3.800e123-s4.000e58 s3.825e123-s3.975e58 s3.850e123-s3.950e58 s3.875e123-s3.925e58 s3.900e123-s3.900e58"
+Ncore="16"
 Nsnapshots="1000"
-raw_par="$HOME/Git/TranSFF/Forcefields/C2_sSOMEeSOME.par"
+raw_par="$HOME/Git/TranSFF/Forcefields/C4/C4_s3.760e120.5-sSOMEeSOME.par"
 rerun_inp="none"                                                                                 # "none" or filename
 GOMC_exe="$HOME/Git/GOMC/GOMC-FSHIFT2-HighPrecisionPDB-StartFrame/bin/GOMC_CPU_NVT"
-Selected_Ts="259.42 174.46 137.97 360.00 360.00 360.00 360.00"                                    # "all" or array
-Selected_rhos="0.4286 0.5571 0.6000 0.0857 0.4286 0.5571 0.6000"                                  # "all" or array
-sig=(3.740 3.742)
-eps=(116.5 117.0)
+Selected_Ts="361.43 241.55 191.34 510.16 510.16 510.16 510.16"                                    # "all" or array
+Selected_rhos="0.4873 0.6335 0.6822 0.0975 0.4873 0.6335 0.6822"                                  # "all" or array
+
+sig=(3.980	3.984	3.988	3.992	3.996	4.000	4.004	4.008	4.012	4.016	4.020)
+eps=(53.0	54.0	55.0	56.0	57.0	58.0	59.0	60.0	61.0	62.0	63.0)
 
 # Plot_heatmap arguments
-MW="30.06904"
-true_data_file_array="$HOME/Git/TranSFF/Data/C2/GONvtRdr_select7.res $HOME/Git/TranSFF/Data/C2/REFPROP_select7.res"                                                               
-true_data_label_array="TraPPE-UA REFPROP"
+MW="58.1222"
+true_data_file_array="$HOME/Git/TranSFF/Data/C4/REFPROP_select7.res"                                                               
+true_data_label_array="REFPROP"
 mbar_file_name_tail_keyword="target.res"
 z_wt="0.5"
 u_wt="0.5"
-heatmap_outfilename_array="${z_wt}Z_${u_wt}U_TraPPE-U.grid ${z_wt}Z_${u_wt}U_REFPROP.grid"
+heatmap_outfilename_array="${z_wt}Z_${u_wt}U_REFPROP.grid"
 sig_increment="0.002"
 eps_increment="1"
 

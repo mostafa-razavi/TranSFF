d69aeaa91ee6507ad567bba369ad61ea44b27180
mostafa-razavi <sr87@zips.uakron.edu>
2019-08-27 06:10:24 -0400

    Old calc_mbar_from_true_data_dev-old.py and plot_mbar_vs_true_data-old.py

---
+++ Scripts/calc_mbar_from_true_data_dev-old.py
@@ -0,0 +1,53 @@
+# This script computes the deviation between MBAR and true data. The data in true_data_file and mbar_data_file should be consistent in terms of order of lines. 
+
+import numpy
+import sys
+
+MW = float(sys.argv[1])
+true_data_file = sys.argv[2]               # The file that is used to minimize the deviations against
+mbar_data_file = sys.argv[3]               # The file that contains MBAR results at ITIC state points
+z_wt = float(sys.argv[4])                  # The weight of Z in score
+u_wt = float(sys.argv[5])                  # The weight of U in score
+
+R_const = 8.31446261815324
+
+# Conversion factors
+atm_to_mpa = 0.101325
+kcalmol_to_kjmol = 4.184
+
+bar_to_mpa = 0.1
+kelvin_to_kjmol = 0.0083
+
+# Import data
+true_data = numpy.loadtxt(true_data_file, skiprows=1, usecols=(0,1,2,5,11))
+true_temp_k = true_data[:,0]
+true_rho_gcc = true_data[:,1]
+true_p_mpa = true_data[:,2] * atm_to_mpa
+true_u_kjmol = true_data[:,3] * kcalmol_to_kjmol
+Nmolec = true_data[:,4]
+
+mbar_data = numpy.loadtxt(mbar_data_file, skiprows=1, usecols=(0,1,3,5))
+mbar_temp_k = mbar_data[:,0]
+mbar_rho_gcc = mbar_data[:,1]
+mbar_u_kjmol = mbar_data[:,2] * kelvin_to_kjmol
+mbar_p_mpa = mbar_data[:,3] * bar_to_mpa
+
+
+# Calcualte properties
+true_u_hat = true_data[:,3] * kcalmol_to_kjmol / Nmolec / R_const / true_temp_k * 1e3
+true_z = true_p_mpa * MW / ( true_rho_gcc  * R_const * true_temp_k )
+true_zminus1overRho = ( true_z - 1 ) / true_rho_gcc
+
+mbar_u_hat = mbar_data[:,2] * kelvin_to_kjmol / Nmolec / R_const / mbar_temp_k * 1e3
+mbar_z = mbar_p_mpa * MW / ( mbar_rho_gcc  * R_const * mbar_temp_k )
+mbar_zminus1overRho = ( mbar_z - 1 ) / mbar_rho_gcc
+
+# Calculate score
+z_dev = ( mbar_zminus1overRho - true_zminus1overRho ) / true_zminus1overRho * 100.0
+u_dev = ( mbar_u_hat - true_u_hat ) / true_u_hat * 100.0
+
+z_aad = numpy.mean(numpy.abs(z_dev))
+u_aad = numpy.mean(numpy.abs(u_dev))
+
+score = z_wt * z_aad + u_wt * u_aad
+print(score)
---
+++ Scripts/plot_mbar_vs_true_data-old.py
@@ -0,0 +1,114 @@
+# This script plots the MBAR vs true data
+
+import matplotlib
+import numpy
+import sys
+import os
+if os.environ.get('DISPLAY','') == '': # then no display found. Use non-interactive Agg backend'
+    matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+MW = float(sys.argv[1])
+true_data_file = sys.argv[2]               # The file that is used to minimize the deviations against, i.e. the target. It could be experimental data or a simulation results of a force field from literature.
+mbar_data_file = sys.argv[3]               # The file that contains MBAR results at ITIC state points
+true_data_label = sys.argv[4]               # Label for true data
+output_figure_filename = sys.argv[5]        # Output file name
+try:
+    dsim_data_file = sys.argv[6]            # If provided, this argument is a file containing direct simulation results
+except:
+    isDSIM = False
+else:
+    isDSIM = True
+
+R_const = 8.31446261815324
+
+# Conversion factors
+atm_to_mpa = 0.101325
+kcalmol_to_kjmol = 4.184
+
+bar_to_mpa = 0.1
+kelvin_to_kjmol = 0.0083
+
+# Import data
+true_data = numpy.loadtxt(true_data_file, skiprows=1, usecols=(0,1,2,5,11))
+true_temp_k = true_data[:,0]
+true_rho_gcc = true_data[:,1]
+true_p_mpa = true_data[:,2] * atm_to_mpa
+true_u_kjmol = true_data[:,3] * kcalmol_to_kjmol
+Nmolec = true_data[:,4]
+
+mbar_data = numpy.loadtxt(mbar_data_file, skiprows=1, usecols=(0,1,3,5,2))
+mbar_temp_k = mbar_data[:,0]
+mbar_rho_gcc = mbar_data[:,1]
+mbar_u_kjmol = mbar_data[:,2] * kelvin_to_kjmol
+mbar_p_mpa = mbar_data[:,3] * bar_to_mpa
+Neff = mbar_data[:,4]
+
+if isDSIM:
+    dsim_data = numpy.loadtxt(dsim_data_file, skiprows=1, usecols=(0,1,2,5,11))
+    dsim_temp_k = dsim_data[:,0]
+    dsim_rho_gcc = dsim_data[:,1]
+    dsim_p_mpa = dsim_data[:,2] * atm_to_mpa
+    dsim_u_kjmol = dsim_data[:,3] * kcalmol_to_kjmol
+
+# Calcualte properties
+true_u_hat = true_data[:,3] * kcalmol_to_kjmol / Nmolec / R_const / true_temp_k * 1e3
+true_z = true_p_mpa * MW / ( true_rho_gcc  * R_const * true_temp_k )
+true_zminus1overRho = ( true_z - 1 ) / true_rho_gcc
+
+mbar_u_hat = mbar_data[:,2] * kelvin_to_kjmol / Nmolec / R_const / mbar_temp_k * 1e3
+mbar_z = mbar_p_mpa * MW / ( mbar_rho_gcc  * R_const * mbar_temp_k )
+mbar_zminus1overRho = ( mbar_z - 1 ) / mbar_rho_gcc
+
+if isDSIM:
+    dsim_u_hat = dsim_data[:,3] * kcalmol_to_kjmol / Nmolec / R_const / dsim_temp_k * 1e3
+    dsim_z = dsim_p_mpa * MW / ( dsim_rho_gcc  * R_const * dsim_temp_k )
+    dsim_zminus1overRho = ( dsim_z - 1 ) / dsim_rho_gcc
+
+# Initiate plot
+plt.figure(num=None, figsize=(24, 7), dpi=100, facecolor='w', edgecolor='w')
+plt.subplots_adjust(left=0.04, bottom=None, right=0.99, top=None, wspace=0.3, hspace=None)
+font = {'weight' : 'normal', 'size' : 14}
+matplotlib.rc('font', **font)    
+
+# (Z-1)/rho vs. rho plot
+plt.subplot(1, 3, 1 )
+plt.xlabel("$\\rho$ [$\mathrm{g/cm^3}$]")
+plt.ylabel("$(Z-1)/\\rho}$ [$\mathrm{cm^3/g}$]")
+plt.scatter(true_rho_gcc, true_zminus1overRho, marker="o", facecolors='none', edgecolors='k', label=true_data_label)
+plt.scatter(mbar_rho_gcc, mbar_zminus1overRho, marker="o", facecolors='none', edgecolors='r', label='MBAR')
+if isDSIM:
+    plt.scatter(dsim_rho_gcc, dsim_zminus1overRho, marker="o", facecolors='none', edgecolors='g', label='Direct dsimulation')
+for i in range(0,len(mbar_rho_gcc)):
+    plt.text(mbar_rho_gcc[i]+0.01, mbar_zminus1overRho[i], str(round(Neff[i],1)), color='red', fontsize=9, alpha=0.9, bbox=dict(color='red', alpha=0.01))
+plt.legend()
+
+# Z vs. 1000/T plot
+plt.subplot(1, 3, 2 )
+plt.xlabel("$1000/T$ [K$^{-1}$]")
+plt.ylabel("$Z$")
+plt.scatter(1000.0 / true_temp_k[:10], true_z[:10], marker="o", facecolors='none', edgecolors='k')
+plt.scatter(1000.0 / true_temp_k[21:], true_z[21:], marker="o", facecolors='none', edgecolors='k')
+plt.scatter(1000.0 / mbar_temp_k[:10], mbar_z[:10], marker="o", facecolors='none', edgecolors='r')
+plt.scatter(1000.0 / mbar_temp_k[21:], mbar_z[21:], marker="o", facecolors='none', edgecolors='r')    
+if isDSIM:
+    plt.scatter(1000.0 / dsim_temp_k[:10], dsim_z[:10], marker="o", facecolors='none', edgecolors='k')
+    plt.scatter(1000.0 / dsim_temp_k[21:], dsim_z[21:], marker="o", facecolors='none', edgecolors='k')
+
+
+# TU^ vs 1000/T plot
+plt.subplot(1, 3, 3 )
+#plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+plt.xlabel("$1000/T$ [K$^{-1}$]")
+plt.ylabel("$T\hat{U}$ [K]")
+plt.scatter(1000.0 / true_temp_k[:10], true_u_hat[:10] * true_temp_k[:10], marker="o", facecolors='none', edgecolors='k')
+plt.scatter(1000.0 / true_temp_k[21:], true_u_hat[21:] * true_temp_k[21:], marker="o", facecolors='none', edgecolors='k')
+plt.scatter(1000.0 / mbar_temp_k[:10], mbar_u_hat[:10] * mbar_temp_k[:10], marker="o", facecolors='none', edgecolors='r')
+plt.scatter(1000.0 / mbar_temp_k[21:], mbar_u_hat[21:] * mbar_temp_k[21:], marker="o", facecolors='none', edgecolors='r')
+if isDSIM:
+    plt.scatter(1000.0 / dsim_temp_k[:10], dsim_u_hat[:10] * dsim_temp_k[:10], marker="o", facecolors='none', edgecolors='k')
+    plt.scatter(1000.0 / dsim_temp_k[21:], dsim_u_hat[21:] * dsim_temp_k[21:], marker="o", facecolors='none', edgecolors='k')
+
+
+plt.savefig(output_figure_filename)
+plt.close()

0360121d753849c7b8575847d6db118d1a322a98
mostafa-razavi <sr87@zips.uakron.edu>
2019-08-27 04:27:00 -0400

    Fixed a name

--- Scripts/RunITIC_GOMC_Parallel.sh
+++ Scripts/RunITIC_GOMC_Parallel.sh
@@ -109,7 +109,7 @@ fi
 
 if [ "$should_run" == "yes" ]; then
 	bash $HOME/Git/TranSFF/Scripts/GONvtRdr/GONvtRdr.sh nvt.inp nvt
-	ndata=$(echo "($RunSteps/$BlockAverageFreq)/2" | bc )
+	ndataskip=$(echo "($RunSteps/$BlockAverageFreq)/2" | bc )
 	nblocks="5"
-	bash $HOME/Git/TranSFF/Scripts/GONvtRdr/GONVT_BlockAvg.sh Blk_${OutputName}_BOX_0.dat $ndata $nblocks
+	bash $HOME/Git/TranSFF/Scripts/GONvtRdr/GONVT_BlockAvg.sh Blk_${OutputName}_BOX_0.dat $ndataskip $nblocks
 fi

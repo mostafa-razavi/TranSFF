5054358a2a334eda83142d0b10ba68083e47c2ab
mostafa-razavi <sr87@zips.uakron.edu>
2019-08-27 05:02:05 -0400

    Fixed a bug

--- Scripts/GONvtRdr/GONVT_BlockAvg.sh
+++ Scripts/GONvtRdr/GONVT_BlockAvg.sh
@@ -30,11 +30,14 @@ do
 		echo $T $rho $stdev > Blocks.std
 	fi
 done
+
+cd $CD
+
 tail -q -n1 $CD/I*/*/*/Blocks.avg > $CD/Blocks.avg
 tail -q -n1 $CD/I*/*/*/Blocks.std > $CD/Blocks.std
 sed -i '1i\T_K RHO_GCC TOT_EN EN_INTER EN_TC EN_INTRA(B) EN_INTRA(NB) EN_ELECT EN_REAL EN_RECIP TOTAL_VIR PRESSURE TOT_MOL TOT_DENS ' $CD/Blocks.avg
 sed -i '1i\T_K RHO_GCC TOT_EN EN_INTER EN_TC EN_INTRA(B) EN_INTRA(NB) EN_ELECT EN_REAL EN_RECIP TOTAL_VIR PRESSURE TOT_MOL TOT_DENS ' $CD/Blocks.std
 
-MW=$(grep -R MW: Files/*.itic | awk '{print$2}')
+MW=$(grep -R MW: $CD/Files/*.itic | awk '{print$2}')
 rm -rf trhozures.res
-python3.6 $HOME/Git/TranSFF/Scripts/GONvtRdr/GONVT_blocks_to_trhozures.py $MW intra Blocks.avg Blocks.std >> trhozures.res # single.avg single.std
+python3.6 $HOME/Git/TranSFF/Scripts/GONvtRdr/GONVT_blocks_to_trhozures.py $MW intra $CD/Blocks.avg $CD/Blocks.std >> $CD/trhozures.res

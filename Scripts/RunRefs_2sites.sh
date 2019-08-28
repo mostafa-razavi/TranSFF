3050ae84d583a3b1a4622c77b128a2ea5b915dd4
mostafa-razavi <sr87@zips.uakron.edu>
2019-08-28 03:39:28 -0400

    Added new IB force field files

--- Scripts/RunRefs_2sites.sh
+++ Scripts/RunRefs_2sites.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 # This script runs GOMC_ITIC_MBAR.sh script for specified sigma and epsilons as two arrays by modifying the raw_par file.
 
-molec_name="C12"
-sig_ch3=(3.700	3.725	3.750	3.775	3.800	3.825	3.850	3.875	3.900)
-sig_ch2=(4.100	4.075	4.050	4.025	4.000	3.975	3.950	3.925	3.900)
+molec_name="IB"
+sig_site1=(3.700	3.725	3.750	3.775	3.800	3.825	3.850	3.875	3.900)
+sig_site2=(4.800	4.775	4.750	4.725	4.700	4.675	4.650	4.625	4.600)
 
-eps_ch3="123"
-eps_ch2="58"
+eps_site1="120"
+eps_site2="13.5"
 
 Scripts_path="$HOME/Git/TranSFF/Scripts"
 Molecules_path="$HOME/Git/TranSFF/Molecules/$molec_name"
-Forcefileds_path="$HOME/Git/TranSFF/Forcefields"
+Forcefileds_path="$HOME/Git/TranSFF/Forcefields/$molec_name"
 
 raw_par="${molec_name}_sSOMEeSOME.par"
 raw_par_path="$Forcefileds_path/$raw_par"
-conf_file="${Scripts_path}/FSHIFT_BULK_LONG.conf"
+conf_file="FSHIFT_BULK_LONG.conf"
 
 i=-1
-for isig_ch3 in "${sig_ch3[@]}"
+for isig_site1 in "${sig_site1[@]}"
 do
     i=$((i+1))
-    isig_ch2=${sig_ch2[i]}
+    isig_site2=${sig_site2[i]}
 
-    sim_name="s${isig_ch3}e${eps_ch3}-s${isig_ch2}e${eps_ch2}"
+    sim_name="s${isig_site1}e${eps_site1}-s${isig_site2}e${eps_site2}"
     par_file_name="${molec_name}_${sim_name}.par"
     par_file_path="${Forcefileds_path}/$par_file_name"
 
     cp  $raw_par_path $par_file_path
 
-    sed -i "s/some_sig_ch3/$isig_ch3/g" $par_file_path
-    sed -i "s/some_sig_ch2/$isig_ch2/g" $par_file_path
+    sed -i "s/some_sig_site1/$isig_site1/g" $par_file_path
+    sed -i "s/some_sig_site2/$isig_site2/g" $par_file_path
 
-    sed -i "s/some_eps_ch3/$eps_ch3/g" $par_file_path
-    sed -i "s/some_eps_ch2/$eps_ch2/g" $par_file_path
+    sed -i "s/some_eps_site1/$eps_site1/g" $par_file_path
+    sed -i "s/some_eps_site2/$eps_site2/g" $par_file_path
             
     mkdir $sim_name
     cd $sim_name

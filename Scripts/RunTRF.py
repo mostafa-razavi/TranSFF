a7a2bd35ee63957399409a0fbd727e7421f98b65
mostafa-razavi <sr87@zips.uakron.edu>
2019-09-01 20:22:04 -0400

    Fixed a bug

--- Scripts/RunTRF.py
+++ Scripts/RunTRF.py
@@ -21,7 +21,7 @@ ub = [3.8, 130]
 
 xy_initial_guess = [3.700,110.0]
 
-result = least_squares(objective_function_2D, xy_initial_guess, jac='2-point', bounds=(lb, ub), method='trf', ftol=1e-08, xtol=1e-08, gtol=1e-08, x_scale=1.0, loss='linear', f_scale=1.0, diff_step=None, tr_solver=None, tr_options={}, jac_sparsity=None, max_nfev=None, verbose=0, args=(), kwargs={})
+result = least_squares(objective_function, xy_initial_guess, jac='2-point', bounds=(lb, ub), method='trf', ftol=1e-08, xtol=1e-08, gtol=1e-08, x_scale=1.0, loss='linear', f_scale=1.0, diff_step=None, tr_solver=None, tr_options={}, jac_sparsity=None, max_nfev=None, verbose=0, args=(), kwargs={})
 
 print(result.x[0], result.x[1])
 

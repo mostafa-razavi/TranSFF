00c69703700a50996edc1f9eecee093a74898273
mostafa-razavi <sr87@zips.uakron.edu>
2019-10-12 19:26:03 -0400

    Modified RunRefs_2sites.sh based on new RunITIC_GOMC_Parallel.sh

--- Scripts/RunRefs_2sites.sh
+++ Scripts/RunRefs_2sites.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 # This script runs GOMC_ITIC_MBAR.sh script for specified sigma and epsilons as two arrays by modifying the raw_par file.
 
-molec_name="C12"
-sig_site1=(3.750	3.775	3.800	3.825	3.850	3.875	3.900	3.925	3.950)
-sig_site2=(4.100	4.075	4.050	4.025	4.000	3.975	3.950	3.925	3.900)
+molecule="C12"
+sig_site1=(3.850    3.850   3.850   3.850   3.850   3.850   3.850   3.850   3.850)
+sig_site2=(3.920	3.940	3.960	3.980	4.000	4.020	4.040	4.060	4.080)
+selected_itic_points="0.5336/547.99 0.6937/368.10 691.00/0.2135 691.00/0.5336 691.00/0.6937"
 
 eps_site1="127"
 eps_site2="60"
 
 Scripts_path="$HOME/Git/TranSFF/Scripts"
-Molecules_path="$HOME/Git/TranSFF/Molecules/$molec_name"
-Forcefileds_path="$HOME/Git/TranSFF/Forcefields/$molec_name"
+Molecules_path="$HOME/Git/TranSFF/Molecules/$molecule"
+Forcefileds_path="$HOME/Git/TranSFF/Forcefields/$molecule"
 
-raw_par="${molec_name}_sSOMEeSOME.par"
+raw_par="${molecule}_sSOMEeSOME2.par"
 raw_par_path="$Forcefileds_path/$raw_par"
 conf_file="FSHIFT_BULK_LONG.conf"
+
 
 i=-1
 for isig_site1 in "${sig_site1[@]}"
@@ -23,7 +25,7 @@ do
     isig_site2=${sig_site2[i]}
 
     sim_name="s${isig_site1}e${eps_site1}-s${isig_site2}e${eps_site2}"
-    par_file_name="${molec_name}_${sim_name}.par"
+    par_file_name="${molecule}_${sim_name}.par"
     par_file_path="${Forcefileds_path}/$par_file_name"
 
     cp  $raw_par_path $par_file_path
@@ -36,6 +38,9 @@ do
             
     mkdir $sim_name
     cd $sim_name
-    bash ${Scripts_path}/RunITIC_GOMC_Parallel.sh $molec_name $par_file_name $conf_file "no"
+        cp $HOME/Git/TranSFF/Molecules/${molecule}/${molecule}_Files.zip .
+        unzip ${molecule}_Files.zip
+        rm ${molecule}_Files.zip     
+        bash ${Scripts_path}/RunITIC_GOMC_Parallel.sh $molecule $par_file_name $conf_file "$selected_itic_points" "no"
     cd ..
 done

5dcd02c1d41667dbb6b81b38cefe8702d69a02dd
mostafa-razavi <sr87@zips.uakron.edu>
2019-11-01 04:41:50 -0400

    Improved RunMultipleMoleculesForFF.sh

--- Scripts/RunMultipleMoleculesForFF.sh
+++ Scripts/RunMultipleMoleculesForFF.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 CD=${PWD}
 
-molecules_array="C2 C4 C8 C12"
+molecules_array="C2 C4 C12"
 Forcefiled_name="MiPPE"
-para_file="$HOME/Git/TranSFF/Forcefields/MiPPE-GEN_Alkanes.par"
-config_filename="VDW_BULK_4M.conf"
+para_file="$HOME/Git/TranSFF/Forcefields/MiPPE-GEN_Alkanes_3.740-118_4.03-62.par"
+config_filename="VDW_BULK_2M.conf"
 
 nblocks="5"
-Nproc=8 #$(nproc)
+Nproc=4 #$(nproc)
 OutputName="nvt"
 
 rm -rf ${CD}/COMMANDS.parallel
@@ -18,7 +18,8 @@ do
     cp $HOME/Git/TranSFF/Molecules/${molec}/${molec}_Files.zip ${CD}/${Forcefiled_name}_${molec}
     cd ${CD}/${Forcefiled_name}_${molec}
     unzip ${molec}_Files.zip
-    eval "bash ~/Git/TranSFF/Scripts/RunITIC_GOMC_Parallel.sh $molec $para_file $config_filename all no"
+    select_itic_points=$(cat $HOME/Git/TranSFF/Molecules/${molec}/${molec}_select5.trho)
+    bash ~/Git/TranSFF/Scripts/RunITIC_GOMC_Parallel.sh $molec $para_file $config_filename "$select_itic_points" no
     cat COMMANDS.parallel >> ${CD}/COMMANDS.parallel
     rm ${molec}_Files.zip
     cd $CD

3355aae077d03295c271deb28bc2c90b3da0bd0d
mostafa-razavi <sr87@zips.uakron.edu>
2019-11-14 16:38:23 -0500

    Updated RunMultipleRefsForMultipleMolecules.sh

--- Scripts/RunMultipleRefsForMultipleMolecules.sh
+++ Scripts/RunMultipleRefsForMultipleMolecules.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 CD=${PWD}
 
-molecules_array="C4 C12 C8"
-ref_array="3.780-127.00_4.030-53.00 3.790-125.00_4.020-56.00 3.800-123.00_4.000-58.00 3.810-121.00_3.990-60.00 3.820-119.00_3.980-63.00"
-config_filename="FSHIFT_BULK_4M.conf"
+molecules_array="IC4 IC5 IC6 23DMB IC8 NP 22DMB 22DMPE"
+
+
+all_ref_array[0]="3.780-123.00_4.730-25.00 3.810-122.00_4.660-16.00 3.840-118.00_4.740-20.00 3.790-120.00_4.680-10.00 3.820-120.00_4.720-18.00" #IC4
+all_ref_array[1]="3.780-123.00_4.730-25.00 3.810-122.00_4.660-16.00 3.840-118.00_4.740-20.00 3.790-120.00_4.680-10.00 3.820-120.00_4.720-18.00" #IC5 
+all_ref_array[2]="3.780-123.00_4.730-25.00 3.810-122.00_4.660-16.00 3.840-118.00_4.740-20.00 3.790-120.00_4.680-10.00 3.820-120.00_4.720-18.00" #IC6 
+all_ref_array[3]="3.780-123.00_4.730-25.00 3.810-122.00_4.660-16.00 3.840-118.00_4.740-20.00 3.790-120.00_4.680-10.00 3.820-120.00_4.720-18.00" #23DMB 
+all_ref_array[4]="3.78-123_4.73-25_6.32-5 3.81-122_4.66-16_6.34-10 3.79-120_4.68-10_6.35-8 3.82-120_4.72-18_6.28-3 3.84-118_4.74-20_6.26-6" #IC8 
+all_ref_array[5]="3.780-123.00_6.320-5.00 3.790-120.00_6.340-10.00 3.810-122.00_6.350-8.00 3.820-120.00_6.280-3.00 3.840-118.00_6.260-6.00" #NP 
+all_ref_array[6]="3.780-123.00_6.320-5.00 3.790-120.00_6.340-10.00 3.810-122.00_6.350-8.00 3.820-120.00_6.280-3.00 3.840-118.00_6.260-6.00" #22DMB 
+all_ref_array[7]="3.780-123.00_6.320-5.00 3.790-120.00_6.340-10.00 3.810-122.00_6.350-8.00 3.820-120.00_6.280-3.00 3.840-118.00_6.260-6.00" #22DMPE 
+
+select_key="all"
+
+config_filename="FSHIFT_BULK_6M.conf"
 Nproc=$(nproc)
 gomc_exe_address="$HOME/Git/GOMC/GOMC-FSHIFT2-SWF-HighPrecisionPDB-StartFrame/bin/GOMC_CPU_NVT"
-
 
 rm -rf ${CD}/COMMANDS.parallel
 molecules_array=($molecules_array)
 ref_array=($ref_array)
+
+i=-1
 for molec in "${molecules_array[@]}"
 do 
+  i=$((i+1))
   mkdir ${CD}/${molec}
+
+  ref_array="${all_ref_array[i]}"
+  ref_array=($ref_array)
 
   for ref in "${ref_array[@]}"
   do
@@ -28,7 +45,11 @@ do
     generate_par_output=$(bash $HOME/Git/TranSFF/Scripts/generate_par3.sh "nokey" "$molec" "here" "$ref" 2 $raw_par)
     par_file_name=$(echo $generate_par_output | awk '{print $2}')
 
-    select_itic_points=$(cat $HOME/Git/TranSFF/Molecules/${molec}/${molec}_select9.trho)
+    if [ "$select_key" == "all" ]; then
+      select_itic_points="all"
+    else
+      select_itic_points=$(cat $HOME/Git/TranSFF/Molecules/${molec}/${molec}_${select_key}.trho)
+    fi
 
     bash $HOME/Git/TranSFF/Scripts/RunITIC_GOMC_Parallel.sh "$molec" ${CD}/${molec}/$ref/$par_file_name $config_filename "$select_itic_points" "$gomc_exe_address" no
 

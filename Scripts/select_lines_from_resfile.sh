7a3d5be1ab1475b1cb06b5738c0f376187051f72
mostafa-razavi <sr87@zips.uakron.edu>
2019-11-27 09:13:35 -0500

    Implemented select_lines_from_resfile.sh

---
+++ Scripts/select_lines_from_resfile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#select_which="1 2 6 10 17 19 21 23 25 27" #select9
+select_which="1 4 10 16 20 24 27" #select6
+select_what="select6"
+forcefield="TraPPE"
+
+for i in */
+do
+    for l in $select_which
+    do
+        sed "${l}q;d" $i/${forcefield}.res >> $i/${forcefield}_${select_what}.res
+    done
+done

e66cf7424386514439c00c2180067dd0da2e719f
mostafa-razavi <sr87@zips.uakron.edu>
2020-02-03 12:32:59 -0500

    Modified RunMultipleMoleculesForFF.sh

--- Scripts/RunMultipleMoleculesForFF.sh
+++ Scripts/RunMultipleMoleculesForFF.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 CD=${PWD}
 
-molecules_array="NP 22DMB 22DMH"
+molecules_array="C2 C3 C4"
 Forcefiled_name="MiPPE"
 para_file="$HOME/Git/TranSFF/Forcefields/MiPPE-GEN_Alkanes.par"
-config_filename="VDW_BULK_6M.conf"
+config_filename="VDW_6M.conf"
 gomc_exe_address="$HOME/Git/GOMC/GOMC-FSHIFT2-SWF-HighPrecisionPDB-StartFrame/bin/GOMC_CPU_NVT"
-select="all"    # either "all" or select keyword e.g. "select5" etc.
+select="all"
 nblocks="5"
 Nproc=8 #$(nproc)
 OutputName="nvt"
@@ -15,9 +15,9 @@ rm -rf ${CD}/COMMANDS.parallel
 molecules_array=($molecules_array)
 for molec in "${molecules_array[@]}"
 do 
-    mkdir ${CD}/${Forcefiled_name}_${molec}
-    cp $HOME/Git/TranSFF/Molecules/${molec}/${molec}_Files.zip ${CD}/${Forcefiled_name}_${molec}
-    cd ${CD}/${Forcefiled_name}_${molec}
+    mkdir ${CD}/${molec}
+    cp $HOME/Git/TranSFF/Molecules/${molec}/${molec}_Files.zip ${CD}/${molec}
+    cd ${CD}/${molec}
     unzip ${molec}_Files.zip
     if [ "$select" == "all" ]; then
         select_itic_points="all"
@@ -38,8 +38,9 @@ BlockAverageFreq=$(grep -R "BlockAverageFreq" $config_file | awk '{print $2}')
 ndataskip=$(echo "($RunSteps/$BlockAverageFreq)/2" | bc )
 for molec in "${molecules_array[@]}"
 do 
-    cd ${CD}/${Forcefiled_name}_${molec}
+    cd ${CD}/${molec}
     bash $HOME/Git/TranSFF/Scripts/GONvtRdr/GONVT_BlockAvg.sh Blk_${OutputName}_BOX_0.dat $ndataskip $nblocks
     bash $HOME/Git/TranSFF/Scripts/GONvtRdr/GONvtRdr.sh nvt.inp ${OutputName}
+    bash $HOME/Git/TranSFF/Scripts/ITIC/plot_coexistence.sh ${molec} trhozures.res $Forcefiled_name "mippe-gcmc" "MiPPE-GCMC"   # last two arguments (lit. extention and label) could be arrays
     cd $CD
 done

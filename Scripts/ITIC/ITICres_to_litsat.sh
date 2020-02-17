de366eff25cb8d571c07200a1d2267cfa1287950
mostafa-razavi <sr87@zips.uakron.edu>
2020-02-17 06:46:20 -0500

    Implemented ITICres_to_litsat.sh and generated *.mippe-itic files for all alkanes

---
+++ Scripts/ITIC/ITICres_to_litsat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ITICres=$1 #"ITIC.out"
+Outname=$2
+rm -rf $Outname
+keyword3="Saturation Info"
+keyword4="Critical Properties"
+sed -n "/$keyword3/,/$keyword4/{/$keyword3/b;/$keyword4/b;p}" $ITICres > "ITIC.sat"
+echo "T[K]	ρL[gcc]	ρV[gcc]	P[Mpa]	   Hvap[KJ/mol]	ρL+/-	ρV+/-	P+/-	Hvap+/-" > $Outname
+cat "ITIC.sat" | tail +4 | awk '{print $3,$7,$9,$5,$10}' >> $Outname

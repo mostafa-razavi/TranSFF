461e731c23d82dddd1b3b88ca73b0e7590e1c814
mostafa-razavi <sr87@zips.uakron.edu>
2020-02-19 07:00:12 -0500

    Added select6sat3lowrho3 ITIC selection for REFPROP MiPPE and TraPPE

--- Scripts/select_lines_from_resfile.sh
+++ Scripts/select_lines_from_resfile.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #select_which="1 2 6 10 17 19 21 23 25 27" #select9
-select_which="1 4 10 16 20 24 27" #select6
-select_what="select6"
-forcefield="REFPROP"
+#select_which="1 4 10 16 20 24 27" #select6
+select_which="1 2 6 10 17 19 21" #select6sat3lowrho3
+select_what="select6sat3lowrho3"
+forcefield="MiPPE"
 
 for i in */
 do
     for l in $select_which
     do
-        sed "${l}q;d" $i/${forcefield}.res >> $i/${forcefield}_${select_what}.res
+        if [ -e "${i}${forcefield}.res" ]; then
+            sed "${l}q;d" ${i}${forcefield}.res >> ${i}${forcefield}_${select_what}.res
+        fi
     done
 done

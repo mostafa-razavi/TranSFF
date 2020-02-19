322dc264156d1c87699090be6d91858991560bd1
mostafa-razavi <sr87@zips.uakron.edu>
2020-02-19 07:55:26 -0500

    Added trho files for select6sat3lowrho3 for all molecules

---
+++ Scripts/trho/generate_trho_file.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+round() {
+  printf "%.${2}f" "${1}"
+}
+
+select_what="select6sat3lowrho3"
+forcefield="MiPPE"
+inFile="${forcefield}_${select_what}.res"
+
+for iMolec in *
+do
+
+    T_IT1=$(grep -R "T_IT:" $HOME/Git/TranSFF/Molecules/${iMolec}/${iMolec}.itic | awk '{print $2}')
+    T_IT2=$(grep -R "T_IT:" $HOME/Git/TranSFF/Molecules/${iMolec}/${iMolec}.itic | awk '{print $3}')
+
+    Ts=($(cat ${iMolec}/$inFile | tail -n +2 | awk '{print$1}'))
+    Rhos=($(cat ${iMolec}/$inFile | tail -n +2 | awk '{print$2}'))
+
+    i=0   
+    string="" 
+    for Ts in "${Ts[@]}"
+    do
+        T=$(round ${Ts[i]} 2)
+        rho=$(round ${Rhos[i]} 4)
+        if [ "$T" == "$T_IT1" ] || [ "$T" == "$T_IT2" ]; then
+            append="$T/$rho"
+        else
+            append="$rho/$T"
+        fi
+        string="$string $append"
+
+        i=$((i+1))
+    done
+
+    echo $string > ${iMolec}/${iMolec}_${select_what}.trho
+
+done
+

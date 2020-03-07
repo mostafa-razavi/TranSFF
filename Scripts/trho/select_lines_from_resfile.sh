752da6a77417df09bf0e46cd489076e200e53463
mostafa-razavi <sr87@zips.uakron.edu>
2020-03-07 10:41:32 -0500

    Added TraPE.res and TraPPE_select9.res files for C2E C3E 1C4E 1C8E IC4E

--- Scripts/trho/select_lines_from_resfile.sh
+++ Scripts/trho/select_lines_from_resfile.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#select_which="1 2 6 10 17 19 21 23 25 27" #select9
+select_which="1 2 6 10 17 19 21 23 25 27" #select9
 #select_which="1 4 10 16 20 24 27" #select6
 #select_which="1 2 6 10 17 19 21" #select6sat3lowrho3
 #select_which="1 2 6 10" #select3sat3
-select_which="1 2 10 17" #select3sat2lowrho1
-select_what="select3sat2lowrho1"
-forcefield="REFPROP"
+#select_which="1 2 10 17" #select3sat2lowrho1
+select_what="select9"
+forcefield="TraPPE"
 
 for i in */
 do

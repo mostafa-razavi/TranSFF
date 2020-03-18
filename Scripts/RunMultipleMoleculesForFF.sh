54deff92879d54a6beb96ee340096a14690fbe1d
mostafa-razavi <sr87@zips.uakron.edu>
2020-03-18 15:47:12 -0400

    Updated RunMultipleMoleculesForFF.sh to generate *.xxx-itic-razavi files

--- Scripts/RunMultipleMoleculesForFF.sh
+++ Scripts/RunMultipleMoleculesForFF.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CD=${PWD}
 
-Forcefiled_name="TraPPE-SWF"
+Forcefield_name="TraPPE-SWF"
 molecules_array="IC4 IC5 IC6 23MB 2MH 25MH 34MH NP IC8 22MB 22MP 22MH 33MH"
 para_file="$HOME/myProjects/GOMC/ITIC/TraPPE-SWF_branched-alkanes_CH3-3.723-101.68_CH2-3.993-51.28_CH1-4.709-12.74_CT-6.222-1.7/TraPPE-SWF_Alkanes_CH3-3.723-101.68_CH2-3.993-51.28_CH1-4.709-12.74_CT-6.222-1.7.par"
 config_filename="FSWITCH_4M_rc1214_light.conf"
@@ -15,10 +15,10 @@ gomc_exe_address="$HOME/Git/GOMC/GOMC-FSHIFT2-SWF-HighPrecisionPDB-StartFrame/bi
 LitsatExt="trappe-gcmc"
 LitsatLabel="TraPPE-GCMC"
 ITIC_trhozures_filename="trhozures.res TraPPE.res"
-ITIC_trhozures_label="$Forcefiled_name TraPPE"
+ITIC_trhozures_label="$Forcefield_name TraPPE"
 trimZ="no-trimZ" 
 trimU="yes-trimU"
-
+Forcefield_ext="trappeswf2-itic-razavi"
 
 #======================================
 rm -rf ${CD}/COMMANDS.parallel
@@ -51,7 +51,8 @@ do
     cd ${CD}/${molec}
     bash $HOME/Git/TranSFF/Scripts/GONvtRdr/GONVT_BlockAvg.sh Blk_${OutputName}_BOX_0.dat $ndataskip $nblocks
     bash $HOME/Git/TranSFF/Scripts/GONvtRdr/GONvtRdr.sh nvt.inp ${OutputName}
-    bash $HOME/Git/TranSFF/Scripts/ITIC/plot_vle.sh ${molec} trhozures.res $Forcefiled_name "$LitsatExt" "$LitsatLabel"   # last two arguments (lit. extention and label) could be arrays
+    bash $HOME/Git/TranSFF/Scripts/ITIC/plot_vle.sh ${molec} trhozures.res $Forcefield_name "$LitsatExt" "$LitsatLabel"   # last two arguments (lit. extention and label) could be arrays
     bash $HOME/Git/TranSFF/Scripts/ITIC/plot_zures.sh ${molec} "$ITIC_trhozures_filename" "$ITIC_trhozures_label" $trimZ $trimU
+    bash $HOME/Git/TranSFF/Scripts/ITIC/ITIC.sh ${molec} trhozures.res ${molec}.${Forcefield_ext}
     cd $CD
 done
